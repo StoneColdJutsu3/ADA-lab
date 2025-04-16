dd78f335623a8d848de375d4306e12f418cd136b
Shaurya Malhotra <145213891+StoneColdJutsu3@users.noreply.github.com>
2025-04-16 12:11:10 +0530

    Update 01knapsackdp.cpp

--- 01knapsackdp.cpp
+++ 01knapsackdp.cpp
@@ -1,36 +1,34 @@
 #include <iostream>
 using namespace std;
 
-const int MAX_N = 100; // Max number of items
-const int MAX_W = 1000; // Max capacity
+const int MAX_N = 100; 
+const int MAX_W = 1000; 
 
 int knapsack(int weights[], int values[], int n, int W) {
     int dp[MAX_N + 1][MAX_W + 1];
 
-    // Initialize DP table: dp[i][w] = max value with first i items and capacity w
     for (int i = 0; i <= n; i++) {
         for (int w = 0; w <= W; w++) {
-            // Base case: no items or no capacity
             if (i == 0 || w == 0)
                 dp[i][w] = 0;
             else if (weights[i - 1] <= w)
                 dp[i][w] = max(
-                    values[i - 1] + dp[i - 1][w - weights[i - 1]], // Take item
-                    dp[i - 1][w]                                   // Don't take item
+                    values[i - 1] + dp[i - 1][w - weights[i - 1]],
+                    dp[i - 1][w]                                   
                 );
             else
-                dp[i][w] = dp[i - 1][w]; // Can't take item
+                dp[i][w] = dp[i - 1][w];
         }
     }
 
-    return dp[n][W]; // Maximum value for n items and capacity W
+    return dp[n][W];
 }
 
 int main() {
     int weights[] = {1, 4, 7, 9};
     int values[] = {10, 30, 50, 60};
-    int n = 4;           // Number of items
-    int W = 8;           // Max capacity of knapsack
+    int n = 4;          
+    int W = 8;          
 
     int maxValue = knapsack(weights, values, n, W);
     cout << "Maximum value in knapsack = " << maxValue << endl;
